e972ffb57c2dca8484f03977ba1462a9e70e09f7
GDLPratya <pratya.khaday@gmail.com>
2019-07-09 14:55:00 +0700

    update readme.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Executable-Jar-Args
+
+using
+
+		java -jar Executable-Jar-Args.jar arg1:555 arg2:tests arg5:ssss arg6:hello arg7:
+
+![Capture](https://user-images.githubusercontent.com/15135199/60869304-ea1b7300-a258-11e9-8bdb-56bd20c202f3.PNG)
--- src/main/java/com/tirmizee/App.java
+++ src/main/java/com/tirmizee/App.java
@@ -2,15 +2,36 @@ package com.tirmizee;
 
 public class App {
 	
+	private static String arg1;
+	private static String arg2;
+	private static String arg3;
+	private static String arg4;
+	private static String arg5;
 	
     public static void main(String...args) {
+    	System.out.println("Send...");
     	System.out.println("Key = Value");
-	    for (String arg : args) {
+	    
+    	for (String arg : args) {
 			String[] parameter = arg.split(":");
 			if (parameter.length == 2) {
 				System.out.println(parameter[0] + " = " + parameter[1]);
+				switch (parameter[0]) {
+					case "arg1" : arg1 = parameter[1]; break;
+					case "arg2" : arg2 = parameter[1]; break;
+					case "arg3" : arg3 = parameter[1]; break;
+					case "arg4" : arg4 = parameter[1]; break;
+					case "arg5" : arg5 = parameter[1]; break;
+				}
 			}
 		}
+	    
+    	System.out.println("Parameter...");
+		System.out.println("arg1 = " + arg1);
+		System.out.println("arg2 = " + arg2);
+		System.out.println("arg3 = " + arg3);
+		System.out.println("arg4 = " + arg4);
+		System.out.println("arg5 = " + arg5);
         System.out.println( "End Task" );
     }
 
